38ddaa6d01f657bdfa73d98215c726d7944354aa
Eduardo Fleitas <edufleitas227@gmail.com>
2026-06-02 15:04:47 -0300

    feat: se realiza la conexión a la BD mongo nest-pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path'; //en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -9,6 +11,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
  rootPath: join(__dirname,'..','public'),
  }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule 
   ],
 })
