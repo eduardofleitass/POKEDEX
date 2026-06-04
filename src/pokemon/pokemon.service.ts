ff15b1597224bfede25e26fe13ca8e947cd6c68f
Eduardo Fleitas <edufleitas227@gmail.com>
2026-06-04 11:38:24 -0300

    feat: FindOneBy se agrega filtros de busqueda de datos BD

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from './entities/pokemon.entity';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId  } from 'mongoose';
 
 @Injectable()
 export class PokemonService {
@@ -19,8 +19,8 @@ async create(createPokemonDto: CreatePokemonDto) {
   return pokemon;
 
   }catch (error){
-    if ( error.code === 11000){
-      throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
+    if ( (error as any).code === 11000){
+      throw new BadRequestException(`Pokemon exists in db ${JSON.stringify((error as any).keyValue)}`);
     }
     console.log(error);
     throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
@@ -30,9 +30,25 @@ async create(createPokemonDto: CreatePokemonDto) {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon:Pokemon | null= null;
+    //ID
+    if ( !isNaN(+term) ){
+      pokemon = await this.pokemonModel.findOne({ no: +term })
+    }
+    //Mongo ID
+      if ( !pokemon && isValidObjectId (term)){
+    pokemon = await this.pokemonModel.findById (term);
   }
+    //Name
+      if (!pokemon){
+        pokemon = await this.pokemonModel.findOne({name: term.toLocaleLowerCase()});
+      }
+    if(!pokemon) throw new NotFoundException(`Pokemon with id , name or no ${"not found"}`)
+    return pokemon; 
+
+  }
+  //MongoID
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
     return `This action updates a #${id} pokemon`;
